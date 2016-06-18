215c292f61591ce44fbe355bb5dc372845b5aec0
martymarkenson <martyamark@gmail.com>
2016-06-17 19:38:16 -0500

    tweaked win/lose animation a little

---
+++ Assets/Scripts/moleculesFormedText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moleculesFormedText : MonoBehaviour {
+	public GameObject canvasObject;
+	private canvasManager canvas;
+	private int compoundGoal, compoundsLeft;
+
+	void Start() {
+		canvas = canvasObject.GetComponent<canvasManager> ();
+		compoundGoal = canvas.getCompoundGoal ();
+	}
+
+	void Update () {
+		compoundsLeft = compoundGoal - canvasManager.compoundsFormed;
+		GetComponent<TextMesh> ().text = compoundsLeft + "\ndiatomic moleculues\n left to form";
+	}
+}
