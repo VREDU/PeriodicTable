21adb3c0126d1fac098015d6dca7bf4a18bbdcd1
martymarkenson <martyamark@gmail.com>
2016-08-20 19:09:33 -0500

    Added volume control

---
+++ Assets/atomQuantity.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class atomQuantity : MonoBehaviour {
+	public GameObject atom;
+	public Transform spawnPoint;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddAtoms() {
+		Instantiate (atom, spawnPoint.position, spawnPoint.rotation);
+	}
+}
---
+++ Assets/idealGasManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class idealGasManager : MonoBehaviour {
+	public Transform boundary;
+	private Vector3 scale;
+	// Use this for initialization
+	void Start () {
+		scale = new Vector3 (.5f, .5f, .5f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void increaseBoundary(){
+		boundary.localScale += scale;
+	}
+
+	public void decreaseBoundary(){
+		boundary.localScale -= scale;
+	}
+}
